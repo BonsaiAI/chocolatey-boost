1f1c6968b75923282fd47e785f85f3b73c4b9c40
Keen Browne <keen.browne@bonsai.ai>
2015-12-08 10:49:47 -0800

    Updated name to bonsai.boost

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 Install-ChocolateyPackage `
-    -packageName 'boost-msvc12' `
+    -packageName 'bonsai.boost' `
     -installerType 'exe' `
     -silentArgs '/VERYSILENT' `
     -url 'http://downloads.sourceforge.net/project/boost/boost-binaries/1.58.0/boost_1_58_0-msvc-12.0-32.exe' `
