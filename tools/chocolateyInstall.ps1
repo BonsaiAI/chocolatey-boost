6fa061c0e50098ab40ddc8c67eb6444bd7ca8f99
Shane Arney <shane.arney@bonsai.ai>
2015-12-09 06:23:20 -0800

    Case 124: Boost installers in ps1 script now hosted on S3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ Install-ChocolateyPackage `
     -packageName 'bonsai.boost' `
     -installerType 'exe' `
     -silentArgs '/VERYSILENT' `
-    -url 'http://downloads.sourceforge.net/project/boost/boost-binaries/1.58.0/boost_1_58_0-msvc-12.0-32.exe' `
-    -url64bit 'http://downloads.sourceforge.net/project/boost/boost-binaries/1.58.0/boost_1_58_0-msvc-12.0-64.exe'
+    -url 'https://s3-us-west-2.amazonaws.com/ai.bonsai.chocolatey-hosting/boost_1_58_0-msvc-12.0-32.exe' `
+    -url64bit 'https://s3-us-west-2.amazonaws.com/ai.bonsai.chocolatey-hosting/boost_1_58_0-msvc-12.0-64.exe'
